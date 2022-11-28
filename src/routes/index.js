0dcee7d26440dc3da19b9e83e7a3bd876b0c8ee6
unknown <jordanhui19@hotmail.com>
2022-11-27 20:33:55 -0500

    added hostname to health check response

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // src/routes/index.js
 
 const express = require('express');
+const { hostname } = require('os');
 
 // Our authorization middleware
 const { authenticate } = require('../authorization');
@@ -21,15 +22,14 @@ const router = express.Router();
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
   res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this...
-    githubUrl: 'https://github.com/jordanhui19/fragments',
-    version,
+      status: 'ok',
+      author: 'Jordan Hui',
+      githubUrl: 'https://github.com/jordanhui19/fragments',
+      version,
+      // Include the hostname in the response
+      hostname: hostname(),
   });
 });
 
